5258d5df63fe1495867ecc95d33963bcff75bd5f
Leonardo Souza <leonardodisouza@gmail.com>
2025-08-20 23:24:34 -0300

    feat: add arrow to show imc result

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function Pessoa(altura, peso) {
-    if (!altura || !peso) {
+    if (!altura && !peso) {
         throw new Error("Altura e peso são obrigatórios");
     }
 
-    this.altura = altura;
-    this.peso = peso;
+    this.altura = parseFloat(altura);
+    this.peso = parseFloat(peso);
 }
 
 function Nutricionista(altura, peso) {
@@ -14,27 +14,31 @@ function Nutricionista(altura, peso) {
     };
 
     this.classificaIMC = function () {
+        document.querySelectorAll("tbody tr").forEach(function (tr) {
+            tr.classList.remove("result");
+        });
+
         var imc = this.imc();
         if (imc < 18.5) {
-            return "Abaixo do peso";
-        }
-        if (imc >= 18.5 && imc < 24.9) {
-            return "Peso normal";
-        }
-        if (imc >= 25 && imc < 29.9) {
-            return "Sobrepeso";
+            document.querySelector(".level1").classList.add("result");
         }
 
-        return "Obesidade";
+        if (imc >= 18.5 && imc < 24.9) {
+            document.querySelector(".level2").classList.add("result");
+        }
+
+        if (imc >= 25 && imc < 29.9) {
+            document.querySelector(".level3").classList.add("result");
+        }
+
+        if (imc >= 30) {
+            document.querySelector(".level4").classList.add("result");
+        }
     };
 }
 Nutricionista.prototype = Object.create(Pessoa.prototype);
 Nutricionista.prototype.constructor = Nutricionista;
 
-function renderizaResultadoIMC(nutricionista) {
-    document.getElementById("imc").innerText =
-        nutricionista.imc().toFixed(2) + " - " + nutricionista.classificaIMC();
-}
 
 function actionCalcularIMCBuilder() {
     var alturaEl = document.getElementById("altura");
@@ -42,15 +46,8 @@ function actionCalcularIMCBuilder() {
 
     return function actionCalcularIMC(evt) {
         evt.preventDefault();
-
-        var nutricionista = new Nutricionista(
-            parseFloat(alturaEl.value),
-            parseFloat(pesoEl.value)
-        );
-        console.log(Nutricionista.prototype.constructor);
-        console.log(nutricionista instanceof Pessoa);
-
-        renderizaResultadoIMC(nutricionista);
+        var nutricionista = new Nutricionista(alturaEl.value, pesoEl.value);
+        nutricionista.classificaIMC();
     }
 }
 
